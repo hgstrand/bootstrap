6e1fdbf1fa970487e057889d1a4fc83c050ba347
Henrik Strand <henrik@Henriks-MacBook-Air-3.local>
2016-12-16 22:44:15 +0100

    make vim dirs if not present

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env bash
 
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -13,6 +13,12 @@ ln -s ~/.dotfiles/.bashrc ~
 ln -s ~/.dotfiles/.bash_profile ~
 ln -s ~/.dotfiles/.vimrc ~
 ln -s ~/.dotfiles/.inputrc ~
+
+# Make backup- and swapdir
+mkdir -p ~/.vim/backups
+mkdir -p ~/.vim/swaps
+mkdir -p ~/.vim/undo
+
 
 source .bashrc
 source .bash_profile
