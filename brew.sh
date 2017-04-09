14717bec1c9765831f60203c7a542ade0136bda9
Henrik Strand <henrik@strand.is>
2017-04-09 09:56:17 +0200

    add arduino and etcher

--- brew.sh
+++ brew.sh
@@ -26,11 +26,15 @@ brew install wget --with-iri
 
 brew cask install alfred
 brew cask install android-studio
+brew cask install apptrap
+brew cask install arduino
 brew cask install caffeine
+brew cask install calibre 
 brew cask install dropbox
 brew cask install easyfind
 brew cask install electrum
 brew cask install electrum-ltc
+brew cask install etcher
 brew cask install flux
 brew cask install google-chrome
 brew cask install iterm2
@@ -41,6 +45,7 @@ brew cask install psequel
 brew cask install seil
 brew cask install soulver
 brew cask install spotify
+brew cask install the-unarchiver 
 brew cask install transmission
 brew cask install tunnelblick
 brew cask install virtualbox
