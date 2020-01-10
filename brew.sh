6bf5e682d8b69a7ab9a1f919188bdd79a18553d2
Henrik Strand <henrik@Henriks-MacBook-Air.local>
2020-01-10 18:06:25 +0100

    new casks

--- brew.sh
+++ brew.sh
@@ -17,8 +17,8 @@ brew upgrade --all
 
 brew install bash
 brew install git
+brew install python
 brew install vim 
-brew install macvim 
 brew install wget
 
 
@@ -26,19 +26,22 @@ brew cask install alfred
 # brew cask install android-studio
 # brew cask install apptrap
 # brew cask install arduino
-brew cask install keepingyouawake
+# brew cask install balenaetcher
 # brew cask install calibre 
 # brew cask install dropbox
 brew cask install easyfind
 brew cask install electrum
 brew cask install electrum-ltc
-# brew cask install etcher
 brew cask install firefox
 brew cask install flux
 brew cask install google-chrome
+brew cask install iina
 brew cask install iterm2
 brew cask install karabiner-elements
+brew cask install keepingyouawake
 # brew cask install kindle
+brew cask install macvim
+brew cask install marked
 brew cask install soulver
 brew cask install spotify
 # brew cask install the-unarchiver 
