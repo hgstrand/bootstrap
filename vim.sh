288f97531f12cc3c2466438ae2a6329e9f637550
Strand <henrik.strand@telia.no>
2020-02-18 17:25:21 +0100

    Added Synctastic

--- vim.sh
+++ vim.sh
@@ -6,8 +6,9 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Install addons with Pathogen
 cd ~/.vim/bundle
-git clone https://github.com/mattn/emmet-vim.git                #Emmet
-git clone https://github.com/vim-airline/vim-airline.git        #Airline
-git clone https://github.com/vim-airline/vim-airline-themes     #Airline themes
-git clone https://github.com/ctrlpvim/ctrlp.vim.git             #Ctrlp
+git clone https://github.com/mattn/emmet-vim.git                    #Emmet
+git clone https://github.com/vim-airline/vim-airline.git            #Airline
+git clone https://github.com/vim-airline/vim-airline-themes         #Airline themes
+git clone https://github.com/ctrlpvim/ctrlp.vim.git                 #Ctrlp
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git  #Synctastic
 
