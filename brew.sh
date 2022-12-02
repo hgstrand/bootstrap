780986711e6e51aa77ada66ea1706aefef6fbb0c
hgstrand <henrik.g.strand@gmail.com>
2022-12-03 00:27:01 +0100

    decomment, changed brew install curl line

--- brew.sh
+++ brew.sh
@@ -9,30 +9,30 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Make sure we’re using the latest Homebrew.
+#Make sure we’re using the latest Homebrew.
 brew update
 
-# Upgrade any already-installed formulae.
+#Upgrade any already-installed formulae.
 brew upgrade
 
 brew install bash
 brew install git
-# brew install python
+#brew install python
 brew install vim 
 brew install wget
 
 
 brew cask install alfred
+brew cask install arctype
 brew cask install bitwarden
-# brew cask install android-studio
-# brew cask install apptrap
-# brew cask install arduino
-# brew cask install balenaetcher
-# brew cask install calibre 
-# brew cask install dropbox
+brew cask install apptrap
+#brew cask install arduino
+#brew cask install balenaetcher
+#brew cask install calibre 
+#brew cask install dropbox
 brew cask install easyfind
 #brew cask install electrum
-brew cask install electrum-ltc
+#brew cask install electrum-ltc
 brew cask install firefox
 brew cask install flux
 brew cask install google-chrome
@@ -40,17 +40,18 @@ brew cask install iina
 brew cask install iterm2
 brew cask install karabiner-elements
 brew cask install keepingyouawake
-# brew cask install kindle
+brew cask install kindle
 brew cask install macvim
 brew cask install marked
+brew cask install obsidian
 brew cask install soulver
 brew cask install spotify
-# brew cask install the-unarchiver 
+#brew cask install the-unarchiver 
 brew cask install transmission
 brew cask install tunnelblick
-# brew cask install virtualbox
-# brew cask install vagrant
-brew cask install vlc
+#brew cask install virtualbox
+#brew cask install vagrant
+#brew cask install vlc
 
 # Installing Command-line File Completion
 git clone https://github.com/pindexis/qfc $HOME/.qfc
