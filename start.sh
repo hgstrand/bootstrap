d31237e47023bfe212c6571532181bf08f89107b
Henrik <henrik.g.strand@gmail.com>
2025-05-23 17:58:30 +0200

    removed vim settings

--- start.sh
+++ start.sh
@@ -5,26 +5,7 @@
 
 
 # Symlink dotfiles
-
 ln -s ~/dev/dotfiles/.zshrc ~
-#ln -s ~/dev/dotfiles/.vimrc ~
-
-### Vim setup ###
-# Make backup- and swapdir
-#mkdir -p ~/.vim/backups
-#mkdir -p ~/.vim/swaps
-#mkdir -p ~/.vim/undo
-
-# Add (solarized, pencil) colors for vim if not present
-if [ ! -f $HOME/.vim/colors/solarized.vim ]; then
-    curl -fLo $HOME/.vim/colors/solarized.vim --create-dirs \
-    https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
-fi
-if [ ! -f $HOME/.vim/colors/pencil.vim ]; then
-    curl -fLo $HOME/.vim/colors/pencil.vim --create-dirs \
-    https://raw.githubusercontent.com/reedes/vim-colors-pencil/master/colors/pencil.vim
-fi
-###
 
 source ~/.zshrc
 
